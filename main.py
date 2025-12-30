ab9fb9d3d63a69d6b35aff5689b7f4fa07b8f3e2
209155548 <lour_s@outlook.com>
2025-12-30 21:41:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 df = pd.read_csv('/content/parkinsons.csv')
 df = df.dropna()
 df.head()
@@ -16,22 +15,13 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2)
 
 from sklearn.svm import SVC
-
 svc = SVC()
-
 svc.fit(x_train, y_train)
 
 
 from sklearn.metrics import accuracy_score
-
 y_pred = model.predict(x_test)
-
 accuracy = accuracy_score(y_test, y_pred)
 print(accuracy)
-
-import joblib
-
-joblib.dump(model, 'my_model.joblib')
-
 
 
